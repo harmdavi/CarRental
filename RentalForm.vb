5ef4863bddd43c2a02da507d094da53f70a64d6b
harmdavi <harmondrum@gmail.com>
2020-10-29 08:14:35 -0600

    Pushing again to make sure that it works

--- RentalForm.vb
+++ RentalForm.vb
@@ -9,7 +9,7 @@ Option Compare Binary
 Public Class RentalForm
     'These where a sad attempt on a dream lost. (basically what I thought would work did not at all)
 
-    'Dim mrKrabs As New List(Of String)
+    'Dim mrKrabs As New List(Of String).
     'Dim number() As String = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
     'Dim letter() As String = {"q", "Q", "w", "W", "e", "E", "r", "R", "t", "T", "y", "Y", "u", "U", "i", "I", "o", "O",
     '    "p", "P", "a", "A", "s", "S", "d", "D", "f", "F", "g", "G", "h", "H", "j", "J", "k", "K", "l", "L", "z", "Z", "x", "X", "c",
